78b44b43d4f0e6cf8657c4885b3699339c57b83c
nolan@thewordnerd.info <nolan@thewordnerd.info>
2009-10-17 22:46:57 +0000

    Add missing resources to .apk.
    --HG--
    extra : convert_revision : 719135e2c0dcda5047ce6500d96895a80a5afebc

--- project/build/SpielProject.scala
+++ project/build/SpielProject.scala
@@ -1,13 +1,33 @@
 import sbt._
+import Process._
 import java.io.File
 
 class SpielProject(info: ProjectInfo) extends AndroidProject(info) {
-  def androidSdkPath = Path.fromFile(new File("/home/nolan/lib/android-sdk-linux_x86-1.6_r1"))
   override def androidPlatformName = "android-1.6"
+
   val rhino = "rhino" % "js" % "1.7R2"
+
   override def proguardOption = """
     -adaptresourcefilenames **.properties
     -keep class * extends org.mozilla.javascript.VMBridge
     -keep public class org.mozilla.javascript.Token
   """
+
+  override def packageTask(signPackage: Boolean) = task {
+    super.packageTask(signPackage)
+    FileUtilities.unzip(classesMinJarPath, outputDirectoryName, GlobFilter("org/mozilla/javascript/resources/*.properties"), log)
+    for(p <- (outputDirectoryName/"org"**"*.properties").get) {
+      (
+        (new java.lang.ProcessBuilder(
+          aaptPath.absolutePath,
+          "add",
+          packageApkPath.absolutePath,
+          p.toString.replace(outputDirectoryName+"/", "")
+        ))
+        directory outputDirectoryName.asFile
+      ) ! log
+    }
+    FileUtilities.clean(outputDirectoryName/"org", log)
+    None
+  }
 }
