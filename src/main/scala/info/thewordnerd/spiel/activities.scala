376b610155ca0f51ba9a39294af6cc506323692e
nolan@thewordnerd.info <nolan@thewordnerd.info>
2009-10-24 05:09:03 +0000

    Beginnings of a UI, and a ContentProvider for scripts.
    --HG--
    extra : convert_revision : eda797d32f8309b41b0b1ee9034825e77da0dc49

---
+++ src/main/scala/info/thewordnerd/spiel/activities.scala
@@ -0,0 +1,27 @@
+package info.thewordnerd.spiel.activities
+
+import android.app.TabActivity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+
+class Main extends TabActivity {
+
+  override protected def onCreate(savedInstanceState:Bundle) {
+    super.onCreate(savedInstanceState)
+    val tabHost = getTabHost()
+    LayoutInflater.from(this).inflate(
+      R.layout.main, tabHost.getTabContentView(), true
+    )
+
+    tabHost.addTab(tabHost.newTabSpec("tab1"
+      ).setIndicator("Scripting"
+      ).setContent(new Intent(this, classOf[Main]))
+    )
+
+    tabHost.addTab(tabHost.newTabSpec("About"
+      ).setIndicator("About"
+      ).setContent(R.id.about)
+    )
+  }
+}
