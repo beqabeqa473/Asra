92dc35875253b6ed81da4a8743014793b19b1992
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-02-05 17:54:54 +0000

    Skeleton of review mode, not yet functional.
    --HG--
    extra : convert_revision : c927d93ca09704b5290aee542ca39029fefd86b3

--- src/main/scala/info/spielproject/spiel/activities.scala
+++ src/main/scala/info/spielproject/spiel/activities.scala
@@ -1,6 +1,6 @@
 package info.spielproject.spiel.activities
 
-import android.app.TabActivity
+import android.app.{Activity, TabActivity}
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -24,4 +24,12 @@ class Main extends TabActivity {
       ).setContent(R.id.about)
     )
   }
+}
+
+class ReviewPanel(text:String) extends Activity {
+
+  override protected def onCreate(savedInstanceState:Bundle) {
+    super.onCreate(savedInstanceState)
+  }
+
 }
