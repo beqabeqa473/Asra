878615b164e67b52a7af967da19161559de8c8e5
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-02-24 00:40:06 +0000

    Add facility for repeated speech every X seconds.
    --HG--
    extra : convert_revision : 33308894a466f0dd581828973a36ac0ddfc3416e

--- src/main/scala/info/spielproject/spiel/TTS.scala
+++ src/main/scala/info/spielproject/spiel/TTS.scala
@@ -2,8 +2,9 @@ package info.spielproject.spiel
 
 import android.content.Context
 import android.speech.tts.TextToSpeech
-import android.speech.tts.TextToSpeech._
+import TextToSpeech._
 import android.util.Log
+import scala.actors.Actor._
 
 protected object TTS extends OnInitListener {
 
@@ -15,6 +16,20 @@ protected object TTS extends OnInitListener {
 
   def onInit(i:Int) {
     tts.setLanguage(java.util.Locale.getDefault)
+    tts.setOnUtteranceCompletedListener(
+      new OnUtteranceCompletedListener {
+        def onUtteranceCompleted(id:String) {
+          repeatedSpeech.get(id) match {
+            case Some(v) =>
+              actor {
+                Thread.sleep(v._1*1000)
+                performRepeatedSpeech(id)
+              }
+            case None =>
+          }
+        }
+      }
+    )
     speak("Welcome to spiel!", true)
   }
 
@@ -30,8 +45,33 @@ protected object TTS extends OnInitListener {
     case _ => Log.e(this.toString, "Invalid text format")
   }
 
-  def stop = {
+  def stop {
     Log.d(this.toString, "Stopping speech")
     tts.stop
   }
+
+  private def speakWithUtteranceID(text:String, uid:String) {
+    val params = new java.util.HashMap[String, String]()
+    params.put("utteranceId", uid) // TODO: Why won't Scala see Engine?
+    tts.speak(text, QUEUE_ADD, params).toString
+  }
+
+  private var repeatedSpeech = collection.mutable.Map[String, Tuple2[Int, String]]()
+
+  private val random = new java.util.Random
+
+  def speakEvery(seconds:Int, text:String) = {
+    val key = random.nextInt.toString
+    repeatedSpeech(key) = (seconds, text)
+    performRepeatedSpeech(key)
+    key
+  }
+
+  def stopRepeatedSpeech(key:String) = repeatedSpeech -= key
+
+  private def performRepeatedSpeech(key:String) = repeatedSpeech.get(key) match {
+    case Some(v) => speakWithUtteranceID(v._2, key)
+    case None =>
+  }
+
 }
