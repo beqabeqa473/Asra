df34228aeb15e0bf30ca0ebe11bccede522c71f7
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-02-24 03:32:23 +0000

    Comment out some debugging code that doesn't need to always run.
    --HG--
    extra : convert_revision : 17e754597c294fa79938acd0ba44993e241db093

--- src/main/scala/info/spielproject/spiel/handlers.scala
+++ src/main/scala/info/spielproject/spiel/handlers.scala
@@ -56,7 +56,7 @@ object Handler {
   }
 
   def handle(e:AccessibilityEvent) {
-    Log.d("spiel", "Event "+e.toString)
+    //Log.d("spiel", "Event "+e.toString)
     nextShouldNotInterruptCalled = false
     var continue = true 
     var alreadyCalled = List[Handler]()
@@ -66,7 +66,7 @@ object Handler {
         if(alreadyCalled.contains(h))
           true
         else {
-          Log.d("spiel", "Dispatching to "+pkg+":"+cls)
+          //Log.d("spiel", "Dispatching to "+pkg+":"+cls)
           alreadyCalled ::= h
           !h(e)
         }
@@ -86,7 +86,7 @@ object Handler {
           if(v._1._2 != "" && continue) {
             try {
               val testClass2 = service.getClassLoader.loadClass(v._1._2)
-              Log.d(this.getClass.toString, testClass2.toString+".isAssignableFrom("+testClass+"): "+testClass2.isAssignableFrom(testClass))
+              //Log.d(this.getClass.toString, testClass2.toString+".isAssignableFrom("+testClass+"): "+testClass2.isAssignableFrom(testClass))
               if(
                 testClass2.isAssignableFrom(testClass) && 
                 (v._1._1 == "" || e.getPackageName == v._1._1)
