416af6f8a976030a08ff7ef2765a3c2978ca8915
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-03-09 21:00:17 +0000

    Add feedback when handle is clicked in launcher.
    --HG--
    extra : convert_revision : c96a6c31cb9e1124876d29acbcdb300cc5707d31

--- src/main/assets/scripts/launcher.js
+++ src/main/assets/scripts/launcher.js
@@ -1,7 +1,14 @@
 forPackage("com.android.launcher");
 forClass(".HandleView", {
+
+  onViewClicked: function() {
+    speak("expanded");
+    return true;
+  },
+
   onViewFocused: function() {
     speak("drawer handle");
     return true;
   }
+
 });
