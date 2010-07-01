1c92259a243dccf63bf2918e5133407346bddcb8
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-07-01 20:40:11 +0000

    Main activity is now tabbed.
    --HG--
    extra : convert_revision : b8a05a3b82f1081c197d0a5dc270add82650d0cc

--- src/main/scala/info/spielproject/spiel/activities.scala
+++ src/main/scala/info/spielproject/spiel/activities.scala
@@ -1,7 +1,23 @@
 package info.spielproject.spiel
 
+import android.app.TabActivity
+import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import android.widget.TabHost
+
+class SpielActivity extends TabActivity {
+  override def onCreate(bundle:Bundle) {
+    super.onCreate(bundle)
+    val host = getTabHost
+
+    host.addTab(host.newTabSpec("preferences")
+      .setIndicator(getString(R.string.preferences))
+      .setContent(new Intent(this, classOf[PreferencesActivity]))
+    )
+
+  }
+}
 
 class PreferencesActivity extends PreferenceActivity {
   override def onCreate(bundle:Bundle) {
