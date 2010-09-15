7f583c46b6f806ca167b5316e22b8d0cc7b72b02
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-09-15 19:50:57 +0000

    Make expanding notification bar much less verbose.
    --HG--
    extra : convert_revision : 3f96e638dc43a8cd6d58ae2ffdca6d120e8c7ec8

--- src/main/assets/scripts/android.js
+++ src/main/assets/scripts/android.js
@@ -15,3 +15,11 @@ forClass("com.android.internal.policy.impl.LockPatternKeyguardView", {
     return true;
   }
 });
+
+forClass("com.android.server.status.StatusBarService$ExpandedDialog", {
+  onWindowStateChanged: function() {
+    speak("Notifications", true);
+    nextShouldNotInterrupt();
+    return true;
+  }
+});
