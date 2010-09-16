7f2c786d6f1f35fae86963ad1411730431e59275
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-09-16 17:24:54 +0000

    Complete documentation for triggers package.
    --HG--
    extra : convert_revision : 045ea0f4c5cef168a912d5d3fdec840a43bb6ce9

--- src/main/scala/info/spielproject/spiel/triggers.scala
+++ src/main/scala/info/spielproject/spiel/triggers.scala
@@ -4,12 +4,18 @@ package triggers
 import android.util.Log
 
 /**
- * Defines a named action to be carried out.
+ * Defines a named action to be carried out by a <code>Trigger</code>.
 */
 
 abstract class Action(val key:String, val name:String, val function:() => Unit) {
   Triggers._actions(key) = this
 }
+
+/**
+ * Holds all defined actions. By creating this class and using the powers of 
+ * reflection, we need only define a new <code>Action</code> within and it 
+ * will be automatically registered.
+*/
 
 class Actions {
 
@@ -31,6 +37,11 @@ abstract class Trigger {
 
   def action = _action
 
+  /**
+   * Binds this <code>Tringger</code> to a specified <code>Action</code>. 
+   * Pass in <code>None</code> to unbind.
+  */
+
   def apply(a:Option[Action]) {
     _action.foreach((act) => uninstall(act.function))
     _action = a
@@ -39,19 +50,42 @@ abstract class Trigger {
     }
   }
 
+  /**
+   * Called when a new <code>Action</code> is installed for this <code>Trigger</code>.
+  */
+
   def install()
+
+  /**
+   * Called with the function that is to be installed from this <code>Trigger</code>.
+  */
 
   def uninstall(f:() => Unit)
 
 }
 
+/**
+ * Convenience singleton for setting up triggers.
+*/
+
 object Triggers {
 
   private[triggers] var _actions:collection.mutable.Map[String, Action] = collection.mutable.Map.empty
 
+  /**
+   * Mapping of action names to triggers.
+  */
+
   def actions = _actions
 
+  /**
+   * Initialize triggers using the specified <code>SpielService</code>.
+  */
+
   def apply(service:SpielService) {
+    // Here's where we iterate through the above <code>Actions</code> class, 
+    // registering all <code>Action</code> classes it contains. Doing this 
+    // obviates the need to manually register new actions on creation.
     val a = new Actions
     a.getClass.getDeclaredClasses.foreach { cls =>
       try {
@@ -60,6 +94,7 @@ object Triggers {
           cons.newInstance(a)
       } catch { case _ => }
     }
+    // Set triggers to the <code>Action</code> specified in <code>Preferences</code>.
     ProximityNear(Preferences.onProximityNear)
     ShakingStarted(Preferences.onShakingStarted)
   }
@@ -67,7 +102,7 @@ object Triggers {
 }
 
 /**
-  * Triggered when the proximity sensor registers something nearby.
+ * Triggered when the proximity sensor registers something nearby.
 */
 
 object ProximityNear extends Trigger {
@@ -79,7 +114,7 @@ object ProximityNear extends Trigger {
 }
 
 /**
-  * Triggers when device is shaken.
+ * Triggers when device is shaken.
 */
 
 object ShakingStarted extends Trigger {
