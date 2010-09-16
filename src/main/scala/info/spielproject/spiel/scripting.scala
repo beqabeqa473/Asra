99b44a4e2a8fec8478d5587ef3c0efdd84699d93
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-09-16 17:10:11 +0000

    Add comments to scripting package.
    --HG--
    extra : convert_revision : 4b614d436e7fceb2ba0a76b9b7acbe0c559eeaf5

--- src/main/scala/info/spielproject/spiel/scripting.scala
+++ src/main/scala/info/spielproject/spiel/scripting.scala
@@ -10,6 +10,10 @@ import org.mozilla.javascript.{Context, Function, RhinoException, ScriptableObje
 import info.spielproject.spiel._
 import handlers.{Callback, Handler}
 
+/**
+ * Handler callback that executes a Rhino script.
+*/
+
 class RhinoCallback(f:Function) extends Callback {
   def apply(e:AccessibilityEvent):Boolean = {
     Context.enter
@@ -26,6 +30,10 @@ class RhinoCallback(f:Function) extends Callback {
   }
 }
 
+/**
+ * Singleton granting convenient access to the Rhino scripting subsystem.
+*/
+
 object Scripter {
 
   private var myCx:Context = null
@@ -34,11 +42,16 @@ object Scripter {
   def context = myCx
   def scope = myScope
 
+  /**
+   * Initialize the scripting subsystem based on the specified service.
+  */
+
   def apply(service:SpielService)  {
     myCx = Context.enter
     myCx.setOptimizationLevel(-1)
     myScope = myCx.initStandardObjects()
 
+    // Inject some Spiel objects into the scripting environment.
     val wrappedHandler = Context.javaToJS(Handler, myScope)
     ScriptableObject.putProperty(myScope, "Handler", wrappedHandler)
 
@@ -48,9 +61,12 @@ object Scripter {
     val wrappedTTS = Context.javaToJS(TTS, myScope)
     ScriptableObject.putProperty(myScope, "TTS", wrappedTTS)
 
+    // Run the given code as a string, reporting the specified filename in 
+    // warnings and errors.
     def run(code:String, filename:String) = try {
       val p = filename.substring(0, filename.lastIndexOf("."))
       val pkg = if(p.startsWith("_")) p.substring(1, p.size) else p
+      // Make this script's package name available in the context.
       myScope.put("__pkg__", scope, pkg)
       myCx.evaluateString(myScope, code, filename, 1, null)
     } catch {
@@ -60,13 +76,16 @@ object Scripter {
       myScope.put("__pkg__", scope, null)
     }
 
+    // Load scripts from /spiel/scripts folder on SD card.
     val spielDir = new File(Environment.getExternalStorageDirectory, "spiel")
     if(!spielDir.isDirectory) spielDir.mkdir
     val scriptsDir = new File(spielDir, "scripts")
     if(!scriptsDir.isDirectory) scriptsDir.mkdir
 
+    // Now that we've created the folder, copy the assets.
     val assets = service.getAssets
 
+    // Handy function to read all data from a stream into a string.
     def readAllAvailable(is:InputStream):String = {
       val a = is.available
       val b = new Array[Byte](a)
@@ -88,7 +107,7 @@ object Scripter {
       case _ =>
     }
 
-
+    // Run a script, optionally directly from the assets folder in the package.
     def runScriptFile(f:String, asset:Boolean = false) = {
       val is = if(asset) 
         assets.open("scripts/"+f)
@@ -105,20 +124,28 @@ object Scripter {
     true
   }
 
+  /**
+   * Register a Handler for a given package and class.
+  */
+
   def registerHandlerFor(pkg:String, cls:String, s:Object) {
     val scr = s.asInstanceOf[ScriptableObject]
     val h = new Handler(pkg, cls)
 
     scr.getIds.foreach { property =>
 
+      // Iterate through object properties, mangling them slightly and 
+      // mapping them to AccessibilityEvent types.
       val id = property.asInstanceOf[String]
       val chars = id.substring(2, id.length).toCharArray
       chars(0) = chars(0).toLower
       val func = new String(chars)
 
+      // Check to ensure that the property name maps to a valid AccessibilityEvent type.
       if(Handler.dispatchers.valuesIterator.contains(func)) {
         val f = scr.get(id, myScope)
         if(f.isInstanceOf[Function])
+          // Map the script to an Android AccessibilityEvent type.
           h.dispatches(func) = new RhinoCallback(f.asInstanceOf[Function])
       } else
         Log.e("spiel", func+" is not a valid handler. Skipping.")
