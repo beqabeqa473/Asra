b29798cfa4eea2ddb3272ab502566e4e2fe129d1
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-09-16 18:37:38 +0000

    Document Preferences.
    --HG--
    extra : convert_revision : ba09ff685e06fd1fd1e33adeac1c49eb0436d39f

--- src/main/scala/info/spielproject/spiel/Preferences.scala
+++ src/main/scala/info/spielproject/spiel/Preferences.scala
@@ -6,16 +6,33 @@ import android.util.Log
 
 import triggers._
 
+/**
+ * Singleton for convenient access to preference values. Also tracks 
+ * preference changes and updates relevant subsystems.
+*/
+
 object Preferences extends SharedPreferences.OnSharedPreferenceChangeListener {
 
   private var prefs:SharedPreferences = null
+
+  /**
+   * Initialized based on the provided <code>SpielService</code>.
+  */
 
   def apply(service:SpielService) {
     prefs = PreferenceManager.getDefaultSharedPreferences(service)
     prefs.registerOnSharedPreferenceChangeListener(this)
   }
 
+  /**
+   * Indicates desired speech engine.
+  */
+
   def speechEngine = prefs.getString("speechEngine", TTS.defaultEngine)
+
+  /**
+   * Sets the desired speech engine.
+  */
 
   def speechEngine_=(e:String) {
     val editor = prefs.edit()
@@ -23,23 +40,63 @@ object Preferences extends SharedPreferences.OnSharedPreferenceChangeListener {
     editor.commit()
   }
 
+  /**
+   * Indicates the desired rate scale.
+  */
+
   def rateScale = prefs.getString("rateScale", "1").toFloat
+
+  /**
+   * Indicates the desired pitch scale.
+  */
 
   def pitchScale = prefs.getString("pitchScale", "1").toFloat
 
+  /**
+   * Indicates whether character echo is enabled.
+  */
+
   def echoByChar = prefs.getBoolean("echoByChar", true)
+
+  /**
+   * Indicates whether word echo is enabled.
+  */
 
   def echoByWord = prefs.getBoolean("echoByWord", false)
 
+  /**
+   * Indicates whether the multi-voice fix is to be used.
+  */
+
   def fixMultivoice = prefs.getBoolean("fixMultivoice", false)
+
+  /**
+   * Indicates whether punctuation speech is to be managed by Spiel.
+  */
 
   def managePunctuationSpeech = prefs.getBoolean("managePunctuationSpeech", false)
 
+  /**
+   * Indicates whether the talking caller ID is enabled.
+  */
+
   def talkingCallerID = prefs.getBoolean("talkingCallerID", true)
+
+  /**
+   * Indicates whether talking voicemail alerts are enabled.
+  */
 
   def voicemailAlerts = prefs.getBoolean("voicemailAlerts", true)
 
+  /**
+   * Indicates whether notifications are to be spoken when the screen is off.
+  */
+
   def speakNotificationsWhenScreenOff = prefs.getBoolean("speakNotificationsWhenScreenOff", true)
+
+  /**
+   * Indicates whether repeated speech alerts occur when the ringer is off.
+  */
 
   def repeatedSpeechWhenRingerOff = prefs.getBoolean("repeatedSpeechWhenRingerOff", false)
 
@@ -48,11 +105,27 @@ object Preferences extends SharedPreferences.OnSharedPreferenceChangeListener {
     case str => Some(Triggers.actions(str))
   }
 
+  /**
+   * Indicates <code>Action</code> to run on proximity nearness.
+  */
+
   def onProximityNear = triggerPreference("onProximityNear")
+
+  /**
+   * Indicates <code>Action</code> to run when shaking starts.
+  */
 
   def onShakingStarted = triggerPreference("onShakingStarted")
 
+  /**
+   * Indicates whether backtraces are to be sent.
+  */
+
   def sendBacktraces = prefs.getBoolean("sendBacktraces", false)
+
+  /**
+   * Indicates whether recent <code>AccessibilityEvent</code>s are to be viewed/logged.
+  */
 
   def viewRecentEvents = prefs.getBoolean("viewRecentEvents", false)
 
