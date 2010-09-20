e8ced5f1f8ff4042de4bbca36d6e25c453e37f6c
nolan@thewordnerd.info <nolan@thewordnerd.info>
2010-09-20 15:43:29 +0000

    Document StateObserver.
    --HG--
    extra : convert_revision : e71059b56cad618c3ae5f85c550a712f65111f58

--- src/main/scala/info/spielproject/spiel/StateObserver.scala
+++ src/main/scala/info/spielproject/spiel/StateObserver.scala
@@ -5,9 +5,22 @@ import android.hardware.{Sensor, SensorEvent, SensorEventListener, SensorManager
 import android.media.AudioManager
 import android.util.Log
 
+/**
+ * Singleton holding a variety of callbacks triggered by <code>StateReactor</code>
+ * when something changes. Generally, these take the following form:
+ *
+ * A list of callback functions.
+ * Methods which add and remove a callback.
+ * Methods which execute the given callbacks, run in response to some event.
+*/
+
 object StateObserver {
 
   private var sensorManager:SensorManager = null
+
+  /**
+   * Initialize this <code>StateReactor</code> based off of the specified <code>SpielService</code>.
+  */
 
   def apply(service:SpielService) {
 
@@ -36,59 +49,129 @@ object StateObserver {
 
   private var callAnsweredHandlers = List[() => Unit]()
 
+  /**
+   * Registers a handler to be run when a call is answered.
+  */
+
   def onCallAnswered(h:() => Unit) = {
     callAnsweredHandlers ::= h
     h
   }
 
+  /**
+   * Executes registered handlers when a call is answered.
+  */
+
   def callAnswered() = callAnsweredHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when call is answered.
+  */
 
   def removeOnCallAnswered(h:() => Unit) = callAnsweredHandlers = callAnsweredHandlers.filterNot(_ == h)
 
   private var callIdleHandlers = List[() => Unit]()
+
+  /**
+   * Registers a handler to be run when a call is idle.
+  */
 
   def onCallIdle(h:() => Unit) = {
     callIdleHandlers ::= h
     h
   }
 
+  /**
+   * Runs registered handlers when a call is idle.
+  */
+
   def callIdle = callIdleHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when call is idle.
+  */
 
   def removeOnCallIdle(h:() => Unit) = callIdleHandlers = callIdleHandlers.filterNot(_ == h)
 
   private var callRingingHandlers = List[(String) => Unit]()
+
+  /**
+   * Registers handler to be called when an incoming call is ringing.
+   *
+   * @param number
+  */
 
   def onCallRinging(h:(String) => Unit) = {
     callRingingHandlers ::= h
     h
   }
 
+  /**
+   * Runs registered handlers when call is ringing.
+   *
+   * @param number
+  */
+
   def callRinging(number:String) = callRingingHandlers.foreach { f => f(number) }
+
+  /**
+   * Removes handler from list to be run when call is ringing.
+  */
 
   def removeCallRinging(h:(String) => Unit) = callRingingHandlers = callRingingHandlers.filterNot(_ == h)
 
   private var messageNoLongerWaitingHandlers = List[() => Unit]()
+
+  /**
+   * Registers handler to be run when voicemail indicator is canceled.
+  */
 
   def onMessageNoLongerWaiting(h:() => Unit) = {
     messageNoLongerWaitingHandlers ::= h
     h
   }
 
+  /**
+   * Runs handlers when voicemail indicator is canceled.
+  */
+
   def messageNoLongerWaiting() = messageNoLongerWaitingHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when voicemail indicator is canceled.
+  */
 
   def removeMessageNoLongerWaiting(h:() => Unit) = messageNoLongerWaitingHandlers = messageNoLongerWaitingHandlers.filterNot(_ == h)
 
   private var messageWaitingHandlers = List[() => Unit]()
 
+  /**
+   * Registers handler to be run when voicemail waiting indicator is active.
+  */
+
   def onMessageWaiting(h:() => Unit) = {
     messageWaitingHandlers ::= h
   }
 
+  /**
+   * Runs registered handlers when voicemail indicator is active.
+  */
+
   def messageWaiting() = messageWaitingHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when voicemail indicator is active.
+  */
 
   def removeMessageWaiting(h:() => Unit) = messageWaitingHandlers = messageWaitingHandlers.filterNot(_ == h)
 
   private var proximityFarHandlers = List[() => Unit]()
+
+  /**
+   * Registers handler to be run when proximity sensor registers nothing 
+   * nearby. Activates or deactivates sensor listener based on whether or 
+   * not any handlers are registered.
+  */
 
   def onProximityFar(h:() => Unit) = {
     proximityFarHandlers ::= h
@@ -96,7 +179,17 @@ object StateObserver {
     h
   }
 
+  /**
+   * Runs registered handlers when proximity sensor registers nothing nearby.
+  */
+
   def proximityFar() = proximityFarHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when proximity sensor registers nothing 
+   * nearby. Activates or deactivates sensor listener based on whether 
+   * handlers are registered.
+  */
 
   def removeProximityFar(h:() => Unit) = {
     proximityFarHandlers = proximityFarHandlers.filterNot(_ == h)
@@ -106,13 +199,27 @@ object StateObserver {
 
   private var proximityNearHandlers = List[() => Unit]()
 
+  /**
+   * Registers handler to be run if the proximity sensor registers anything 
+   * nearby. Activates or deactivates sensor listener accordingly.
+  */
+
   def onProximityNear(h:() => Unit) = {
     proximityNearHandlers ::= h
     proximitySensorEnabled = true
     h
   }
 
+  /**
+   * Run handlers when proximity sensor registers something nearby.
+  */
+
   def proximityNear() = proximityNearHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when proximity sensor registers 
+   * something nearby. Activates or deactivates sensor listener accordingly.
+  */
 
   def removeProximityNear(h:() => Unit) = {
     proximityNearHandlers = proximityNearHandlers.filterNot(_ == h)
@@ -122,38 +229,83 @@ object StateObserver {
 
   private var ringerModeChangedHandlers = List[(String) => Unit]()
 
+  /**
+   * Registers handler to be run when ringer mode is changed.
+   *
+   @param mode Either "normal", "silent" or "vibrate"
+  */
+
   def onRingerModeChanged(h:(String) => Unit) = {
     ringerModeChangedHandlers ::= h
     h
   }
 
+  /**
+   * Run registered handlers when ringer mode changes.
+   *
+   * @param mode Either "normal", "silent" or "vibrate"
+  */
+
   def ringerModeChanged(mode:String) = ringerModeChangedHandlers.foreach { f => f(mode) }
+
+  /**
+   * Remove handler from being run when ringer mode changes.
+  */
 
   def removeRingerModeChanged(h:(String) => Unit) = ringerModeChangedHandlers = ringerModeChangedHandlers.filterNot(_ == h)
 
   private var screenOffHandlers = List[() => Unit]()
+
+  /**
+   * Register handler to be run when screen is turned off.
+  */
 
   def onScreenOff(h:() => Unit) = {
     screenOffHandlers ::= h
     h
   }
 
+  /**
+   * Run registered handlers when screen is turned off.
+  */
+
   def screenOff() = screenOffHandlers.foreach { f => f() }
+
+  /**
+   * Remove handler from being run when screen turns off.
+  */
 
   def removeScreenOff(h:() => Unit) = screenOffHandlers = screenOffHandlers.filterNot(_ == h)
 
   private var screenOnHandlers = List[() => Unit]()
+
+  /**
+   * Register handler to be run when screen turns on.
+  */
 
   def onScreenOn(h:() => Unit) = {
     screenOnHandlers ::= h
     h
   }
 
+  /**
+   * Run registered handlers when screen turns on.
+  */
+
   def screenOn() = screenOnHandlers.foreach { f => f() }
+
+  /**
+   * Remove handler from being run when screen turns off.
+  */
 
   def removeScreenOn(h:() => Unit) = screenOnHandlers = screenOnHandlers.filterNot(_ == h)
 
   private var shakingStartedHandlers = List[() => Unit]()
+
+  /**
+   * Register a handler to be run when device starts shaking. Activates or 
+   * deactivates sensor listener accordingly.
+  */
 
   def onShakingStarted(h:() => Unit) = {
     shakingStartedHandlers ::= h
@@ -161,7 +313,16 @@ object StateObserver {
     h
   }
 
+  /**
+   * Runs registered handlers when device starts shaking.
+  */
+
   def shakingStarted() = shakingStartedHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when device starts shaking. Activates or 
+   * deactivates sensor listener accordingly.
+  */
 
   def removeShakingStarted(h:() => Unit) = {
     shakingStartedHandlers = shakingStartedHandlers.filterNot(_ == h)
@@ -171,13 +332,27 @@ object StateObserver {
 
   private var shakingStoppedHandlers = List[() => Unit]()
 
+  /**
+   * Registers handler to be run when device stops shaking. Activates or 
+   * deactivates sensor listener accordingly.
+  */
+
   def onShakingStopped(h:() => Unit) = {
     shakingStoppedHandlers ::= h
     shakerEnabled = true
     h
   }
 
+  /**
+   * Runs registered handlers when device stops shaking.
+  */
+
   def shakingStopped() = shakingStoppedHandlers.foreach { f => f() }
+
+  /**
+   * Removes handler from being run when device stops shaking. Activates or 
+   * deactivates sensor listener accordingly.
+  */
 
   def removeShakingStopped(h:() => Unit) = {
     shakingStoppedHandlers = shakingStoppedHandlers.filterNot(_ == h)
@@ -185,10 +360,13 @@ object StateObserver {
       shakerEnabled = false
   }
 
+  // Make enabling/disabling sensors look like setting boolean values.
+
   private var _shakerEnabled = false
 
   def shakerEnabled = _shakerEnabled
 
+  // This may need tweaking, but this seems like a good value for an intentional shake.
   private val shakerThreshold = 2.6d*SensorManager.GRAVITY_EARTH*SensorManager.GRAVITY_EARTH
 
   private val shaker = new SensorEventListener {
