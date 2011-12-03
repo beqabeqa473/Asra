d6358a1e54fb15a2e9e91fb2218e60e918996964
nolan <nolan>
2011-12-03 16:30:04 +0000

    Cleaned up telephony code and removed support for Android 1.6.
    --HG--
    extra : convert_revision : d98061a8ad4b344556998d11cdc61eb4ddccf9c4

--- src/main/scala/TelephonyListener.scala
+++ src/main/scala/TelephonyListener.scala
@@ -2,7 +2,6 @@ package info.spielproject.spiel
 
 import android.content.{ContentResolver, Context}
 import android.net.Uri
-import android.os.Build.VERSION
 import android.provider.{Contacts, ContactsContract}
 import android.telephony.{PhoneStateListener, TelephonyManager}
 import TelephonyManager._
@@ -14,53 +13,27 @@ import android.util.Log
 
 object TelephonyListener extends PhoneStateListener {
 
-  private var service:SpielService = null
+  private var context:Context = null
 
   /**
-   * Initialize this <code>TelephonyListener</code> based on the specified <code>SpielService</code>.
+   * Initialize this <code>TelephonyListener</code> based on the specified <code>Context</code>.
   */
 
-  def apply(s:SpielService) {
-    service = s
-    val manager = service.getSystemService(Context.TELEPHONY_SERVICE).asInstanceOf[TelephonyManager]
+  def apply(c:Context) {
+    context = c
+    val manager = context.getSystemService(Context.TELEPHONY_SERVICE).asInstanceOf[TelephonyManager]
     manager.listen(this, PhoneStateListener.LISTEN_CALL_STATE|PhoneStateListener.LISTEN_MESSAGE_WAITING_INDICATOR)
   }
 
-  private def cursorFor(uri:Uri) = service.getContentResolver.query(uri, null, null, null, null)
-
-  // Resolve the number to a contact where possible. Handles differing API versions.
+  // Resolve the number to a contact where possible.
 
   private def resolve(number:String) = {
-    if(VERSION.SDK_INT >= 5)
-      resolveV5(number)
-    else {
-      val uri= Uri.withAppendedPath(Contacts.Phones.CONTENT_FILTER_URL, Uri.encode(number))
-      val cursor = cursorFor(uri)
-      var name = number
-      if(cursor.getCount > 0) {
-        while(cursor.moveToNext) {
-          name = cursor.getString(cursor.getColumnIndex(Contacts.PeopleColumns.DISPLAY_NAME))
-        }
-      }
-      name
-    }
-  }
-
-  private def resolveV5(number:String) = {
-    val uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number))
-    var name = ""
-    val cursor = if(number != null && number != "") {
-      name = number
-      Option(cursorFor(uri))
-    } else {
-      name = "Unknown"
-      None
-    }
-    cursor.foreach { c =>
-      if(c.getCount > 0) {
-        while(c.moveToNext) {
-          name = c.getString(c.getColumnIndex(DisplayNameFix.DISPLAY_NAME))
-        }
+    val uri= Uri.withAppendedPath(Contacts.Phones.CONTENT_FILTER_URL, Uri.encode(number))
+    val cursor = context.getContentResolver.query(uri, null, null, null, null)
+    var name = number
+    if(cursor.getCount > 0) {
+      while(cursor.moveToNext) {
+        name = cursor.getString(cursor.getColumnIndex(Contacts.PeopleColumns.DISPLAY_NAME))
       }
     }
     name
