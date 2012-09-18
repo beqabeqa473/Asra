5329ad1ec9a051862c111ed77eaa5fadde41e17b
nolan <nolan>
2012-09-18 15:52:00 +0000

    Add text property to RichEvent.
    --HG--
    extra : convert_revision : c34b7a1c55289e30645ea913f17501fc3ea3fe7f

--- src/main/scala/RichEvent.scala
+++ src/main/scala/RichEvent.scala
@@ -7,23 +7,25 @@ import android.view.accessibility.AccessibilityEvent
 
 class RichEvent(e:AccessibilityEvent) {
 
+  lazy val text = Option(e.getText).map(_.toList).getOrElse(Nil).map(_.toString)
+
   def utterances(addBlank:Boolean = true, stripBlanks:Boolean = false, guessLabelIfTextMissing:Boolean = false, guessLabelIfContentDescriptionMissing:Boolean = false, guessLabelIfTextShorterThan:Option[Int] = None, providedText:Option[String] = None):List[String] = {
     var rv = List[String]()
-    val txt = Option(e.getText).map(_.toList).getOrElse(Nil).filterNot(_ == null).map(_.toString).mkString("\n") match {
+    val t = text.filterNot(_ == null).mkString("\n") match {
       case "" => List()
       case v => v.split("\n").toList
     }
-    val text = if(stripBlanks)
-      txt.filterNot(_.trim.length == 0)
-    else txt
+    val txt:List[String] = if(stripBlanks)
+      t.filterNot(_.trim.length == 0)
+    else t
     var blankAdded = false
     providedText.map(rv ::= _).getOrElse {
-      if(text.size == 0 && e.getContentDescription == null && addBlank) {
+      if(txt.size == 0 && e.getContentDescription == null && addBlank) {
         blankAdded = true
         rv ::= ""
       }
     }
-    rv :::= text
+    rv :::= txt
     if(e.getContentDescription != null && e.getContentDescription != "")
       rv match {
         case hd :: Nil if(hd.toLowerCase.trim == e.getContentDescription.toString.toLowerCase.trim) =>
@@ -31,24 +33,22 @@ class RichEvent(e:AccessibilityEvent) {
           rv ::= e.getContentDescription.toString
       }
     def removeBlank() = if(blankAdded) rv = rv.tail
-    if(VERSION.SDK_INT >= 14) {
-      if(guessLabelIfTextMissing && e.getText.length == 0)
+    if(guessLabelIfTextMissing && e.getText.length == 0)
+      rv :::= Option(e.getSource).flatMap(_.label).map(_.getText.toString).map { v =>
+        removeBlank()
+        List(v)
+      }.getOrElse(Nil)
+    else if(guessLabelIfContentDescriptionMissing && e.getContentDescription == null)
+      rv :::= Option(e.getSource).flatMap(_.label).map(_.getText.toString).map { v =>
+        removeBlank()
+        List(v)
+      }.getOrElse(Nil)
+    else guessLabelIfTextShorterThan.foreach { v =>
+      if(VERSION.SDK_INT >= 16 || text.length < v)
         rv :::= Option(e.getSource).flatMap(_.label).map(_.getText.toString).map { v =>
           removeBlank()
           List(v)
         }.getOrElse(Nil)
-      else if(guessLabelIfContentDescriptionMissing && e.getContentDescription == null)
-        rv :::= Option(e.getSource).flatMap(_.label).map(_.getText.toString).map { v =>
-          removeBlank()
-          List(v)
-        }.getOrElse(Nil)
-      else guessLabelIfTextShorterThan.foreach { v =>
-        if(VERSION.SDK_INT >= 16 || text.length < v)
-          rv :::= Option(e.getSource).flatMap(_.label).map(_.getText.toString).map { v =>
-            removeBlank()
-            List(v)
-          }.getOrElse(Nil)
-      }
     }
     rv
   }
