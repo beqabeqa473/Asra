1597879f20448fea5e1694c9619d7064e1082392
Nolan Darilek <nolan@thewordnerd.info>
2013-09-07 17:14:02 -0500

    Toast exception message before report is sent.

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -2,6 +2,7 @@ package info.spielproject.spiel
 
 import org.acra._
 import annotation._
+import org.scaloid.common._
 
 import events._
 
@@ -10,10 +11,11 @@ import events._
   mailTo = "spiel@thewordnerd.info",
   customReportContent = Array(ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.CUSTOM_DATA, ReportField.STACK_TRACE, ReportField.LOGCAT)
 )
-class Application extends android.app.Application {
+class Application extends android.app.Application with Helpers {
   override def onCreate() {
     ACRA.init(this)
     UnhandledException += { t:Throwable =>
+      toast(t.getMessage)(this)
       if(Preferences.sendBacktraces)
         ErrorReporter.getInstance.handleException(t, false)
     }
